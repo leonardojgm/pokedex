d1c1ccef1f063845a50e082f89656c59cd87e352
leonardojgm <cookie.maia@gmail.com>
2024-10-26 15:08:28 -0300

    Correção de acesso ao Detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let offset = 0;
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-            <a href="detail?number=${pokemon.number}">
+            <a href="detail.html?number=${pokemon.number}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
